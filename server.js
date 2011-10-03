a4107ef899490fcd7acf37f48296f94aa7498e7e
John Mertens <john.mertens@gmail.com>
2011-10-02 20:29:54 -0700

    Whitespace.

--- server.js
+++ server.js
@@ -27,65 +27,52 @@ app.configure(function(){
 app.post('/', function(req, res){
   var tropo = new TropoWebAPI();
 
-	var givenAddress = req.body.session.initialText;
-	console.log(givenAddress);
-	var response = '', bbox, path;
+  var givenAddress = req.body.session.initialText;
+  console.log(givenAddress);
+  var response = '', bbox, path;
 
-	geo.geocoder(geo.google, givenAddress, false, function(formattedAddress, lat, lng) {
-	  if(lat && lng) {
-	    response = "You are at "+lng+', '+lat;
-	    bbox = getBbox({coords: { latitude: lat, longitude: lng } });
-//	    console.log('_design/geo/_spatial/full?bbox='+bbox);
+  geo.geocoder(geo.google, givenAddress, false, function(formattedAddress, lat, lng) {
+    if(lat && lng) {
+      response = "You are at "+lng+', '+lat;
+      bbox = getBbox({coords: { latitude: lat, longitude: lng } });
+//      console.log('_design/geo/_spatial/full?bbox='+bbox);
       path = ['_design', 'geo', '_spatial', 'full?bbox='+bbox].join('/');
-	    db.query('GET',path, function(err, markets) {
-	      console.log(err);
-	      //console.log(data);
-	      if(!err) {
-	        _.each(markets, function(el, idx) {
-	          //console.log(el.value.x+', '+el.value.y);
-	          el.distance = quickDist(lat, lng, el.geometry.coordinates[1], el.geometry.coordinates[0]);
-	        });
-	        markets.sort(function(a, b) { return  a.distance - b.distance; });
+      db.query('GET',path, function(err, markets) {
+        console.log(err);
+        //console.log(data);
+        if(!err) {
+          _.each(markets, function(el, idx) {
+            //console.log(el.value.x+', '+el.value.y);
+            el.distance = quickDist(lat, lng, el.geometry.coordinates[1], el.geometry.coordinates[0]);
+          });
+          markets.sort(function(a, b) { return  a.distance - b.distance; });
 
           _.each(markets, function(m, idx) {
             response += m.value.MarketName + ' is '+m.distance.toFixed(2)+' miles away.';
             console.log(m.value.MarketName + ' is '+m.distance.toFixed(2)+' miles away.');
           });
-	      }
-	      wrapitup();
-	    });
-	  } else {
-	    response = "Sorry, but we can't find that address."
-	    wrapitup();
-	  }
+        }
+        wrapitup();
+      });
+    } else {
+      response = "Sorry, but we can't find that address."
+      wrapitup();
+    }
   });
-
-	// Use the say method https://www.tropo.com/docs/webapi/say.htm
-//	tropo.say("I love you, "+initialText);
-
-  // // Demonstrates how to use the base Tropo action classes.
-  // var say = new Say("Please enter your 5 digit zip code.");
-  // var choices = new Choices("[5 DIGITS]");
-  //
-  // // Action classes can be passes as parameters to TropoWebAPI class methods.
-  // // use the ask method https://www.tropo.com/docs/webapi/ask.htm
-  // tropo.ask(choices, 3, false, null, "foo", null, true, say, 5, null);
-  // // use the on method https://www.tropo.com/docs/webapi/on.htm
-  // tropo.on("continue", null, "/answer", true);
 
   var wrapitup = function() {
     tropo.say(response);
-	  res.send(TropoJSON(tropo));
+    res.send(TropoJSON(tropo));
   }
 
 });
 
 app.post('/answer', function(req, res){
-	// Create a new instance of the TropoWebAPI object.
-	var tropo = new TropoWebAPI();
-	tropo.say("Your zip code is " + req.body['result']['actions']['interpretation']);
+  // Create a new instance of the TropoWebAPI object.
+  var tropo = new TropoWebAPI();
+  tropo.say("Your zip code is " + req.body['result']['actions']['interpretation']);
 
-	res.send(TropoJSON(tropo));
+  res.send(TropoJSON(tropo));
 });
 
 var port = process.env.PORT || 8000;
